7b01c9739626e7a876bb4980547ef1d4c0ed0b47
MobbLee <t1eete@163.com>
2024-09-16 19:40:19 +0800

    Add files via upload

---
+++ chapter 1/task1.java
@@ -0,0 +1,13 @@
+public class task1 {  
+    public static void main(String[] args) {    
+        double width = 4.5;  
+        double height = 7.9;  
+  
+        double area = width * height;  
+  
+        double perimeter = 2 * (width + height);  
+  
+        System.out.println("the area is: " + area);  
+        System.out.println("the perimeter is: " + perimeter);  
+    }  
+}
